6d79033ea06d15075c97fdc3b921c77f8b89dc8e
Kunle Dev <Kunle2000@gmail.com>
2025-05-03 12:41:53 +0100

    fix: correct Granian usage by passing main:app as positional argument for both IPv6 and IPv4 bindings

--- apps/api_gw/start.sh
+++ apps/api_gw/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-granian --interface asgi --app main:app --host :: --port ${PORT:-8000} &
-granian --interface asgi --app main:app --host 0.0.0.0 --port ${PORT:-8000} 
+granian --interface asgi --host :: --port ${PORT:-8000} main:app &
+granian --interface asgi --host 0.0.0.0 --port ${PORT:-8000} main:app 
