ead2701316e380afecf6d91c6b2dbc79aa539854
Kunle Dev <Kunle2000@gmail.com>
2025-05-03 14:06:25 +0100

    chore: use dynamic PORT env variable for both IPv6 and IPv4 bindings with Granian

--- apps/api_gw/start.sh
+++ apps/api_gw/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-granian --interface asgi --host :: --port 8000 main:app &
-granian --interface asgi --host 0.0.0.0 --port 8000 main:app 
+granian --interface asgi --host :: --port ${PORT:-8000} main:app &
+granian --interface asgi --host 0.0.0.0 --port ${PORT:-8000} main:app 
