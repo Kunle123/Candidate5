627a33a71c434ac822b69dccbfb0f41cbf2b3918
Kunle Dev <Kunle2000@gmail.com>
2025-07-02 19:56:41 +0100

    chore: dummy change to trigger redeploy for user service

--- apps/user_service/app/init_db.py
+++ apps/user_service/app/init_db.py
@@ -1,6 +1,8 @@
 from app.models import Base
 from app.db import engine
 
+# Dummy change to trigger redeploy
+
 if __name__ == "__main__":
     print("Creating all tables...")
     Base.metadata.create_all(bind=engine)
