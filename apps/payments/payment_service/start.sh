b6fa70f2e0170e5685e447f880d9391df1f25e40
Kunle Dev <Kunle2000@gmail.com>
2025-09-12 16:04:00 +0100

    chore(logging): set uvicorn log-level to debug and enable access log in start.sh

--- apps/payments/payment_service/start.sh
+++ apps/payments/payment_service/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-uvicorn main:app --host :: --port ${PORT:-8000} &
-uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000} 
+uvicorn main:app --host :: --port ${PORT:-8000} --log-level debug --access-log &
+uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000} --log-level debug --access-log 
